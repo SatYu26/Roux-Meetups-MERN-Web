d542097560a26be990e82c93b12ad5a83330cc79
SatYu26 <goyalsatyam8@gmail.com>
2021-09-22 19:10:09 +0530

    creating a list page

--- routes/speakers.js
+++ routes/speakers.js
@@ -7,7 +7,7 @@ module.exports = (params) => {
 
   router.get('/', async (req, res) => {
     const speakers = await speakersService.getList();
-    return res.json(speakers);
+    res.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
   });
 
   router.get('/:shortname', (req, res) => res.send(`Detail Page of ${req.params.shortname}`));
