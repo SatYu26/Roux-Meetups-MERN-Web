d67ea57b4e1ea316c8bb05c148eadb79f8bb6241
SatYu26 <goyalsatyam8@gmail.com>
2021-09-23 01:51:42 +0530

    Partials and lists

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,9 @@ module.exports = (params) => {
   const { speakersService } = params;
 
   router.get('/', async (req, res) => {
+    const artwork = await speakersService.getAllArtwork();
     const topSpeakers = await speakersService.getList();
-    res.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers });
+    res.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers, artwork });
   });
 
   router.use('/speakers', speakersRoute(params));
--- routes/speakers.js
+++ routes/speakers.js
@@ -7,10 +7,14 @@ module.exports = (params) => {
 
   router.get('/', async (req, res) => {
     const speakers = await speakersService.getList();
-    res.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
+    const artwork = await speakersService.getAllArtwork();
+    res.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers, artwork });
   });
 
-  router.get('/:shortname', (req, res) => res.send(`Detail Page of ${req.params.shortname}`));
-
+  router.get('/:shortname', async (req, res) => {
+    const speaker = await speakersService.getSpeaker(req.params.shortname);
+    const artwork = await speakersService.getArtworkForSpeaker();
+    res.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker, artwork });
+  });
   return router;
 };
